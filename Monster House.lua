e85434d641c36cb98762c921996e05f31411f281
lunjesse <30811237+lunjesse@users.noreply.github.com>
2018-10-21 01:03:29 -0400

    Update Monster House.lua
    * Made percent resemble what I did with Sabre Wulf (used for loops if the same address was responsible for multiple flags)
    * Added pointer to x/y so it's accurate even out of bounds
    * Split percentage into different components to make debugging easier
    * Gave names to the misc flags.

--- Monster House.lua
+++ Monster House.lua
@@ -1,7 +1,7 @@
 memory.usememorydomain("EWRAM")
-gui.DrawNew("native")
-client.setwindowsize(2) --Sets size so that everything fits
-client.SetGameExtraPadding(0, 0, 0, 20)
+-- gui.DrawNew("native")
+-- client.setwindowsize(2) --Sets size so that everything fits
+-- client.SetGameExtraPadding(0, 0, 0, 20)
 local peppermints = 0x3DE9 --6%+ total
 local bottles = 0x3E11 --3% total
 local brassHeart = 0x3E13 --6% total
@@ -15,114 +15,129 @@ local gear = 0x4735 --12% total + 3% at gear+1
 local characters = {'DJ','Chowder','Jenny'}
 local water = {0x37D0,0x37D2,0x37D4}  --Water supply
 local water2 = {0x3DF6,0x3DF8,0x3DFA} --Water capacity; only updates when gun is upgrade via chest
+local pointerx = 0xB3D8
+local player = {x = 0, y = 0, address = 0}
+local character = 0x3DE8
 
 --Checks the bit of 'mem' at position 'pos' and returns 'value' if it's 1
 function addFlags(mem,pos,value)
-if bit.check(mem,pos) then return value
-else return 0 end
+	if bit.check(mem,pos) then return value
+	else return 0 end
+end
+
+function check_percent(address, percent,...)
+--checks address for bits from ..., then if it exists add percent to result.
+	local l_address = memory.read_s8(address)
+	local result = 0
+	for i, v in ipairs({...}) do
+		if bit.check(l_address, v) then result = result + percent end
+	end
+	return result
 end
 
 function percent()
-  local percent = 0
+	--splitting percentage for debug
+	local misc_p, key_item_p, toy_p, watergun_p, gear_p, story_p = 0,0,0,0,0,0
+	local percent_value = 0
+	local percent = 0
   --Checks if the player has the peppermints
-  percent = memory.read_s8(peppermints) + percent
+	misc_p = memory.read_s8(peppermints) + misc_p
   --Check if the player has all 3 bottles (1% each)
-  percent = addFlags(memory.read_s8(bottles),0,1) + percent --All Bottles are 1%
-  percent = addFlags(memory.read_s8(bottles),1,2) + percent --But getting the 2nd bottle makes the value for the above 0
+	misc_p = addFlags(memory.read_s8(bottles),0,1) + misc_p --All Bottles are 1%
+	misc_p = addFlags(memory.read_s8(bottles),1,2) + misc_p --But getting the 2nd bottle makes the value for the above 0
   --Check if the player has a brass heart (2% each)
-  percent = memory.read_s8(brassHeart) * 2 + percent
+	misc_p = memory.read_s8(brassHeart) * 2 + misc_p
   --Check if the player has the Candy box (1%)
-  percent = addFlags(memory.read_s8(candyBox),1,1) + percent
+	misc_p = addFlags(memory.read_s8(candyBox),1,1) + misc_p
   --Checks if the player has the following: Fuse (3%), Dynamite (3%), Elevator Gear (3%), Walkie-Talkie (1%), Flashlight (1%)
-  percent = addFlags(memory.read_s8(keyItems),3,1) + percent  --Flashlight
-  percent = addFlags(memory.read_s8(keyItems),4,1) + percent  --Walkie-Talkie
-  percent = addFlags(memory.read_s8(keyItems),5,3) + percent  --Elevator Gear
-  percent = addFlags(memory.read_s8(keyItems),6,3) + percent  --Dynamite
-  percent = addFlags(memory.read_s8(keyItems),7,3) + percent  --Fuse
+	percent_value = 1
+	key_item_p = key_item_p + check_percent(keyItems,percent_value,3,4)		--Flashlight, Walkie-Talkie
+	percent_value = 3
+	key_item_p = key_item_p + check_percent(keyItems,percent_value,5,6,7)	--Elevator Gear, Dynamite, Fuse
   --Checks if the player has the following: Rover's bone (2%), RC Car (2%), Chicken doll (2%), Boomerang (2%), Chowder's basketball (2%), Skull's action figure (2%)
-  percent = addFlags(memory.read_s8(toys),2,2) + percent --Skull's action figure
-  percent = addFlags(memory.read_s8(toys),3,2) + percent --Chowder's basketball
-  percent = addFlags(memory.read_s8(toys),4,2) + percent --Boomerang
-  percent = addFlags(memory.read_s8(toys),5,2) + percent --Chicken doll
-  percent = addFlags(memory.read_s8(toys),6,2) + percent --RC Car
-  percent = addFlags(memory.read_s8(toys),7,2) + percent --Rover's bone
+	percent_value = 2
+	toy_p = toy_p + check_percent(toys,percent_value,2,3,4,5,6,7)	--Skull's action figure, Chowder's basketball, Boomerang, Chicken doll, RC Car, Rover's bone
   --Checks if the player has the following: Watergun ugrade (DJ 1, 1%), ?? (1%), Wendy the Dancing Walrus doll (2%), Jenny's wagon (2%), Tricycle (2%),  Penguin doll (2%),  Bone's kite (2%)
-  percent = addFlags(memory.read_s8(toys2),0,2) + percent --Bone's kite
-  percent = addFlags(memory.read_s8(toys2),1,2) + percent --Penguin doll
-  percent = addFlags(memory.read_s8(toys2),2,2) + percent --Tricycle
-  percent = addFlags(memory.read_s8(toys2),3,2) + percent --Jenny's wagon
-  percent = addFlags(memory.read_s8(toys2),4,2) + percent --Wendy the Dancing Walrus doll
-  percent = addFlags(memory.read_s8(toys2),6,0) + percent --Doombringer unlocked; placeholder for this script
-  percent = addFlags(memory.read_s8(toys2),7,1) + percent --Watergun upgrade for DJ
-  --Checks if the player has the following: Watergun ugrade (Jenny 1, 2%), Watergun ugrade (Jenny 2, 2%), Watergun ugrade (Jenny 3, 2%), Watergun upgrade (Chowder 1, 2%), Watergun upgrade (Chowder 2, 2%), Watergun upgrade (Chowder 3, 2%), Watergun ugrade (DJ 2, 2%), Watergun ugrade (DJ 3, 2%)
-  percent = addFlags(memory.read_s8(waterguns),0,2) + percent --Watergun upgrade for DJ
-  percent = addFlags(memory.read_s8(waterguns),1,2) + percent --Watergun upgrade for DJ
-  percent = addFlags(memory.read_s8(waterguns),2,2) + percent --Watergun upgrade for Chowder
-  percent = addFlags(memory.read_s8(waterguns),3,2) + percent --Watergun upgrade for Chowder
-  percent = addFlags(memory.read_s8(waterguns),4,2) + percent --Watergun upgrade for Chowder
-  percent = addFlags(memory.read_s8(waterguns),5,2) + percent --Watergun upgrade for Jenny
-  percent = addFlags(memory.read_s8(waterguns),6,2) + percent --Watergun upgrade for Jenny
-  percent = addFlags(memory.read_s8(waterguns),7,2) + percent --Watergun upgrade for Jenny
+	+ check_percent(toys2,percent_value,0,1,2,3,4)				--Bone's kite, Penguin doll, Tricycle, Jenny's wagon, Wendy the Dancing Walrus doll
+	--Watergun upgrade for DJ
+	--Checks if the player has the following: Watergun ugrade (Jenny 1, 2%), Watergun ugrade (Jenny 2, 2%), Watergun ugrade (Jenny 3, 2%), Watergun upgrade (Chowder 1, 2%), Watergun upgrade (Chowder 2, 2%), Watergun upgrade (Chowder 3, 2%), Watergun ugrade (DJ 2, 2%), Watergun ugrade (DJ 3, 2%)
+	watergun_p = watergun_p + addFlags(memory.read_s8(toys2),7,1) + check_percent(waterguns,percent_value,0,1,2,3,4,5,6,7)
   --Checks if the player has the following: Trash can (1%), Slingshot upgrade (3%), Slingshot (1%), Cold Syrup (1%), Water balloon upgrade (3%), Water balloon (1%), Brass Key (1%)
-  percent = addFlags(memory.read_s8(gear),0,1) + percent  --Camera
-  percent = addFlags(memory.read_s8(gear),1,1) + percent  --Brass Key
-  percent = addFlags(memory.read_s8(gear),2,1) + percent  --Water balloon
-  percent = addFlags(memory.read_s8(gear),3,3) + percent  --Water balloon upgrade
-  percent = addFlags(memory.read_s8(gear),4,1) + percent  --Cold Syrup
-  percent = addFlags(memory.read_s8(gear),5,1) + percent  --Slingshot
-  percent = addFlags(memory.read_s8(gear),6,3) + percent  --Slingshot upgrade
-  percent = addFlags(memory.read_s8(gear),7,1) + percent  --Trash Can
-  percent = addFlags(memory.read_s8(gear+1),0,3) + percent  --Camera upgrade
+	percent_value = 1
+	gear_p = gear_p + check_percent(gear,percent_value,0,1,2,4,5,7) -- Camera, Brass Key, Water balloon, Cold Syrup, Slingshot, Trash Can
+	percent_value = 3
+	gear_p = gear_p + check_percent(gear,percent_value,3,6) + addFlags(memory.read_s8(gear+1),0,3)	--Water balloon, Slingshot & Camera upgrades
   --Checks the story events done
-  if memory.read_s8(events) > 14 then percent = percent + 1 end
-  if memory.read_s8(events) > 18 then percent = percent + 1 end
-  if memory.read_s8(events) > 24 then percent = percent + 1 end
-  if memory.read_s8(events) > 30 then percent = percent + 2 end
-  if memory.read_s8(events) > 32 then percent = percent + 1 end
-  if memory.read_s8(events) > 37 then percent = percent + 2 end
-  if memory.read_s8(events) > 41 then percent = percent + 1 end
-  if memory.read_s8(events) > 45 then percent = percent + 1 end
-  if memory.read_s8(events) > 50 then percent = percent + 2 end
-  if memory.read_s8(events) > 54 then percent = percent + 3 end
-  if memory.read_s8(events) > 57 then percent = percent + 1 end
-  if memory.read_s8(events) > 62 then percent = percent + 1 end
-  gui.text(0, client.screenheight()-42, 'Percent: '..percent)
+	if memory.read_s8(events) > 14 then story_p = story_p + 1 end
+	if memory.read_s8(events) > 18 then story_p = story_p + 1 end
+	if memory.read_s8(events) > 24 then story_p = story_p + 1 end
+	if memory.read_s8(events) > 30 then story_p = story_p + 2 end
+	if memory.read_s8(events) > 32 then story_p = story_p + 1 end
+	if memory.read_s8(events) > 37 then story_p = story_p + 2 end
+	if memory.read_s8(events) > 41 then story_p = story_p + 1 end
+	if memory.read_s8(events) > 45 then story_p = story_p + 1 end
+	if memory.read_s8(events) > 50 then story_p = story_p + 2 end
+	if memory.read_s8(events) > 54 then story_p = story_p + 3 end
+	if memory.read_s8(events) > 57 then story_p = story_p + 1 end
+	if memory.read_s8(events) > 62 then story_p = story_p + 1 end
+	percent = misc_p + key_item_p + toy_p + watergun_p + gear_p + story_p
+	gui.text(0, client.screenheight()-32, 'misc: '..misc_p..' key: '..key_item_p..' toy '..toy_p..' gun '..watergun_p..' gear '..gear_p..' story '.. story_p)
+	gui.text(0, client.screenheight()-42, 'Percent: '..percent)
+	gui.text(0, client.screenheight()-52, 'events '..memory.read_s8(events))
 end
 
 function miscflags()
-  local y = client.screenheight()-10
-  gui.text(0, y-18, 'Flags: ')
-  --Checks if the player has obtained the Camera flash at least once
-  if bit.check(memory.read_s8(candyBox),7) then gui.drawRectangle(0, y, 4, 10, 'WHITE', 'WHITE') end
-  --Checks if the player has obtained candy at least once
-  if bit.check(memory.read_s8(candyBox),2) then gui.drawRectangle(5, y, 4, 10, 'RED', 'RED') end
-  --Checks if the player has obtained soda at least once
-  if bit.check(memory.read_s8(candyBox),3) then gui.drawRectangle(10, y, 4, 10, 'GREEN', 'GREEN') end
-  --Checks if the player has obtained marbles at least once
-  if bit.check(memory.read_s8(candyBox),6) then gui.drawRectangle(15, y, 4, 10, 'BLUE', 'BLUE') end
-  --Checks if the player has obtained water at least once
-  if bit.check(memory.read_s8(keyItems),0) then gui.drawRectangle(20, y, 4, 10, 'AQUA', 'AQUA') end
-  --Checks if the player has obtained water bottle at least once
-  if bit.check(memory.read_s8(gear+1),1) then gui.drawRectangle(25, y, 4, 10, 'DARKCYAN', 'DARKCYAN') end
-  --Checks if the player has unlocked the Toys tab
-  if bit.check(memory.read_s8(keyItems),2) then gui.drawRectangle(30, y, 4, 10, 'DARKKHAKI', 'DARKKHAKI') end
+--[[
+The following do not contribute to percentage. They instead indicate if certain events have been achieved:
+* Unlocked doombringer (not saved in SRAM sadly)
+* Checks if the player has obtained the Camera flash at least once
+* Checks if the player has obtained candy at least once
+* Checks if the player has obtained soda at least once
+* Checks if the player has obtained marbles at least onced
+* Checks if the player has obtained water at least once
+* Checks if the player has obtained water bottle at least once
+* Checks if the player has unlocked the Toys tab
+ ]]--
+	local doombringer = bit.check(memory.read_s8(toys2),6)
+	local camera_flash = bit.check(memory.read_s8(candyBox),7)
+	local candy = bit.check(memory.read_s8(candyBox),2)
+	local soda = bit.check(memory.read_s8(candyBox),3)
+	local marble = bit.check(memory.read_s8(candyBox),6)
+	local water_drop = bit.check(memory.read_s8(keyItems),0)
+	local bottle = bit.check(memory.read_s8(gear+1),1)
+	local toy_tab = bit.check(memory.read_s8(keyItems),2)
+end
+
+function update()
+	local ptr = memory.read_s32_le(pointerx)
+	if ptr > 0x02000000 and ptr < 0x03000000 then
+		return ptr - 0x02000000	--Minus (0x02000000) since BizHawk memory domains
+	end
+	return nil
 end
 
 function displaychar()
-  local x = (memory.read_u32_le(0xF9D8)+30720)/65536.0  --Adds 30720 for actual X
-  local y = (memory.read_u32_le(0xF9DC)+20480)/65536.0  --Adds 20480 for actual Y
-  gui.text(0, 60, 'X: '..string.format('%.5f',x) ..' Y: '..string.format('%.5f',y))
-  local char = memory.read_s8(0x3DE8)
-  if char < 3 then  -- just in case
-  local supply = memory.read_s16_le(water[char+1])  --Char uses 0,1,2 but lua arrays are 1,2,3
-  local capacity = memory.read_s16_le(water2[char+1])
-  gui.text(0, 75, characters[char+1]..'('..supply..'/'..capacity..')')
+	local x,y = 0,0
+	local ptr = update()
+	if ptr ~= nil then
+		player.x = memory.read_u32_le(ptr + 0x14)
+		player.y = memory.read_u32_le(ptr + 0x18)
+		player.address = ptr
+		x = string.format('%.5f',player.x/65536.0)
+		y = string.format('%.5f',player.y/65536.0)
+	end
+	local l_char = memory.read_s8(character)
+	gui.text(0, 60, 'X: '..x..' Y: '..y)
+	if l_char < 3 then  -- just in case
+		local supply = memory.read_s16_le(water[l_char+1])  --Char uses 0,1,2 but lua arrays are 1,2,3
+		local capacity = memory.read_s16_le(water2[l_char+1])
+		gui.text(0, 75, characters[l_char+1]..'('..supply..'/'..capacity..')')
   end
 end
 
 while true do
   percent()
-  miscflags()
+  -- miscflags()
   displaychar()
   emu.frameadvance()
 end
