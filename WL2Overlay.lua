bd9f9a27a4a408c93d7ead0342928dde6589b266
lunjesse <30811237+lunjesse@users.noreply.github.com>
2020-03-24 00:32:15 -0400

    Update WL2Overlay.lua
    1. Added more tile types into the table
    2. Changed the comparison to using tables
    3. Added stage ids

--- WL2Overlay.lua
+++ WL2Overlay.lua
@@ -4,6 +4,7 @@ memory.usememorydomain("WRAM")
 local x, y, camx, camy, tref, ttype, ttypehigh, tcolor, warpdest
 local address = {
 	gbc = {
+		npc_base = 0x0000,
 		x = 0x153c,
 		y = 0x153a,
 		camx = 0x660,
@@ -15,6 +16,7 @@ local address = {
 	gb = {
 		x = 0x053c,
 		y = 0x053a,
+		npc_base = 0x1000,
 		camx = 0x1652,
 		camy = 0x1651,
 		tref = 0x16EB,
@@ -25,56 +27,118 @@ local address = {
 local tiles = {
 	gbc = {
 		[0x47bc] = "Switch platform",
-		[0x49cb] = "Slideable slope (left)", --Press down to roll
+		[0x493a] = "Slideable slope (left)", --From Stage 5
 		[0x495e] = "Slideable slope (right)", --Press down to roll
+		[0x49cb] = "Slideable slope (left)", --Press down to roll
 		[0x4a10] = "Platform",
 		[0x4a33] = "Platform (NPC)", --NPCs can walk on them, you cannot
+		[0x4a5d] = "Platform",	--From Stage 5
 		[0x4a70] = "Breakable",	--(ttype>0x4a70) and (ttype<0x4d9f) or (ttype==0x4a10) 
+		[0x4a72] = "Breakable", --From Stage 5
 		[0x4a7e] = "Breakable", --From Stage 34
+		[0x4b62] = "Breakable", --From Stage 5
+		[0x4b66] = "Breakable", --From Stage 3
+		[0x4b6a] = "Breakable", --From Stage 8
+		[0x4bc6] = "Breakable", --From Stage 5
+		[0x4bca] = "Breakable", --From Stage 3
+		[0x4bce] = "Breakable", --From Stage 8
 		[0x4bd2] = "Breakable", --From Stage 34
 		[0x4b6e] = "Breakable", --From Stage 34
+		[0x4ce7] = "Breakable", --From Stage 2
 		[0x4cef] = "Breakable", --From Stage 34
+		[0x4cf3] = "Breakable", --From Stage 0
+		[0x4cf7] = "Breakable", --From Stage 1
+		[0x4cfb] = "Breakable", --From Stage 0
+		[0x4cff] = "Breakable", --From Stage 0
+		[0x4d03] = "Breakable", --From Stage 0
+		[0x4d6b] = "Breakable (Fire)",--From Stage 26
+		[0x4d6f] = "Breakable (Fire)",--From Stage 6
+		[0x4d73] = "Breakable (Food)",--From Stage 8
+		[0x4d9c] = "Breakable (NPC)", --throw npcs to destroy them
 		[0x4d9f] = "Breakable",
-		[0x4da0] = "Breakable (NPC)", --throw npcs to destroy them
+		[0x4da0] = "Breakable (NPC)", --throw npcs to destroy them; Fromm stage 25
+		[0x4da4] = "Breakable (NPC)", --throw npcs to destroy them
 		[0x4e8a] = "Water",
-		[0x4fbe] = "Minigame",	--varies by stage
-		[0x4ecd] = "Minigame",	--varies by stage
+		[0x4ea7] = "Warp",		--From Stage 1
+		[0x4eba] = "Warp",		--From Stage 5
+		[0x4ecd] = "Door",		--From Stage 0
 		[0x4edb] = "Door",
 		[0x4ef6] = "Boss door",	--varies by stage
 		[0x4f3a] = "Exit",
 		[0x4f60] = "Secret exit",
+		[0x4f86] = "Door",		--From Stage 2
+		[0x4f1f] = "Door",		--From Stage 5
+		[0x4fbe] = "Minigame",	--varies by stage
+		[0x4fda] = "Minigame",	--From Stage 2
 		[0x4ffb] = "Switch",
-		[0x50ff] = "Water Current",
+		[0x50ff] = "Water",		--Current
+		[0x510f] = "Water",		--Current
+		[0x511f] = "Water",		--Current
+		[0x5236] = "Spike",		--From stage 36
 		[0x53bb] = "Light"		--Cures zombieism
 	},
 	gb = {
 		[0x47bc] = "Switch platform",
+		[0x493a] = "Slideable slope (left)", --From Stage 5
 		[0x49cb] = "Slideable slope (left)",
 		[0x495e] = "Slideable slope (right)",
 		[0x4a10] = "Platform",
 		[0x4a33] = "Platform (NPC)", --NPCs can walk on them, you cannot
+		[0x4a5d] = "Platform",	--From Stage 5
 		[0x4a70] = "Breakable",	--(ttype>0x4a70) and (ttype<0x4d9f) or (ttype==0x4a10) 
 		[0x4ae1] = "Breakable", --From Stage 34
+		[0x4b66] = "Breakable", --From Stage 3
+		[0x4b6a] = "Breakable", --From Stage 8
 		[0x4b6e] = "Breakable", --From Stage 34
+		[0x4bce] = "Breakable", --From Stage 8
 		[0x4bd2] = "Breakable", --From Stage 34
+		[0x4bca] = "Breakable", --From Stage 3
+		[0x4ce7] = "Breakable",	--From Stage 2
+		[0x4cef] = "Breakable",	--From Stage 4; apparently could be invisible
 		[0x4cf3] = "Breakable",	--From Stage 0
+		[0x4cf7] = "Breakable",	--From Stage 1
 		[0x4cfb] = "Breakable",	--From Stage 0
 		[0x4cff] = "Breakable",	--From Stage 0
 		[0x4d03] = "Breakable",	--From Stage 0
+		[0x4d64] = "Breakable (Fire)",
+		[0x4d6c] = "Breakable (Fire)", --Need to recheck
 		[0x4d9f] = "Breakable",
 		[0x4d95] = "Breakable (NPC)", --throw npcs to destroy them
 		[0x4d99] = "Breakable (NPC)", --throw npcs to destroy them
 		[0x4e83] = "Water",
+		[0x4ea0] = "Warp",		--From Stage 1
+		[0x4eb3] = "Warp",		--From Stage 5
 		[0x4fb7] = "Minigame",
 		[0x4ec6] = "Door",		--From Stage 0
 		[0x4ed4] = "Door",
 		[0x4eef] = "Boss door",
+		[0x4f18] = "Door",		--From Stage 5
 		[0x4f33] = "Exit",
 		[0x4f59] = "Secret exit",
-		[0x4cef] = "Breakable (Invisible)",
+		[0x4f7f] = "Door",		--From Stage 2
+		[0x4fd3] = "Minigame",	--From Stage 2
 		[0x4ff4] = "Switch",
 		[0x50fb] = "Water",
+		[0x510b] = "Water",		--Current
 		[0x5374] = "Light"		--Cures zombieism
+	},
+	colors = {
+		["Door"] = "DARKGRAY",
+		["Boss door"] = "Green",
+		["Minigame"] = "PURPLE",
+		["Exit"] = "CYAN",
+		["Secret exit"] = "GOLD",
+		["Water"] = "BLUE",
+		["Warp"] = "DIMGRAY",
+		["Platform"] = "WHITE",
+		["Platform (NPC)"] = "LIGHTGRAY",
+		["Slideable slope (left)"] = "BROWN",
+		["Slideable slope (right)"] = "BROWN",
+		["Breakable"] = "PINK",
+		["Breakable (NPC)"] = "DEEPPINK",
+		["Breakable (Fire)"] = "DEEPPINK",
+		["Breakable (Food)"] = "DEEPPINK",
+		["Light"] = "LIME"
 	}
 }
 
@@ -116,14 +180,16 @@ end
 
 -- Original source: https://pastebin.com/DFGBfiEF
 -- Written by mugg, modified by me to use WRAM instead.
-function display_npc(x, y, camx, camy)
-	for i = 0x1000, 0x10FF, 32 do
+function display_npc(base, x, y, camx, camy)
+	for i = base, base+0xFF, 32 do
 		 if memory.readbyte(i+0) > 0 then	--coins grabbed seems to make it 2 or 3
 			local xenemy = memory.read_u16_le(i+5)
 			local yenemy = memory.read_u16_le(i+3)
+			local id = memory.readbyte(i+9)
 			local ecamx = (xenemy-x)+camx
 			local ecamy = (yenemy-y)+camy
 			gui.drawBox(ecamx-16,ecamy-32,ecamx,ecamy-16,0x8611759E)
+			gui.pixelText(ecamx-16,ecamy-39,id)
 			gui.pixelText(ecamx-16,ecamy-32,"X:"..xenemy)
 			gui.pixelText(ecamx-16,ecamy-25,"Y:"..xenemy)
 		end
@@ -140,14 +206,14 @@ while true do
 	else
 		game_address = address.gb
 		game_tiles = tiles.gb
-		
 	end
 	x = mainmemory.read_u16_be(game_address.x) -- position in level
 	y = mainmemory.read_u16_be(game_address.y)
 	camx = mainmemory.readbyte(game_address.camx) -- position relative to upper left camera edge
 	camy = mainmemory.readbyte(game_address.camy)
 	tref = mainmemory.read_u16_be(game_address.tref)
-	display_npc(x, y, camx, camy)
+	stageid = memory.readbyte(game_address.stageid)
+	display_npc(game_address.npc_base, x, y, camx, camy)
 	-- warpdest = mainmemory.readbyte(game_address.warpdest) -- sector coordinates for a warp (??)
 	-- 160x144
 	-- gui.drawText(3,130,string.format("%X",bit.rshift(warpdest,4))..' '..string.format("%X",bit.band(warpdest,0xF)))
@@ -160,36 +226,8 @@ while true do
 				-- if (ttype==0x4ecd) or (ttype==0x4edb) or (ttype==0x4f3a) or (ttype==0x4f60) then -- door, minigame, exit
 					-- tcolor = 'GREEN' 
 				if game_tiles[ttype] ~= nil then
-					if (game_tiles[ttype]=="Door") then --Regular door
-						tcolor = 'SILVER' 
-					elseif (game_tiles[ttype]=="Boss door") then --Boss door
-						tcolor = 'GREEN'
-					elseif (game_tiles[ttype]=="Minigame") then -- Minigame
-						tcolor = 'PURPLE'
-					elseif (game_tiles[ttype]=="Exit") then -- exit
-						tcolor = 'CYAN'
-					elseif (game_tiles[ttype]=="Secret exit") then --secret exit
-						tcolor = 'GOLD'
-						console.log("!!")
-					elseif (game_tiles[ttype]=="Water") then -- water
-						tcolor = 'BLUE'
-					elseif (game_tiles[ttype]=="Platform") then -- platform
-						tcolor = 'WHITE'
-					elseif (game_tiles[ttype]=="Platform (NPC)") then -- platform
-						tcolor = 'GREY'
-					elseif (game_tiles[ttype]=="Slideable slope (left)") or (game_tiles[ttype]=="Slideable slope (right)")  then -- platform
-						tcolor = 'BROWN'
-					elseif (ttype>0x4a70) and (ttype<0x4d9f) then -- breakable
-						tcolor = 'PINK'
-					elseif (game_tiles[ttype]=="Breakable") then 
-						tcolor = 'PINK'
-					elseif (game_tiles[ttype]=="Breakable (NPC)") then
-						tcolor = 'DEEPPINK'
-					elseif (game_tiles[ttype]=="Light") then
-						tcolor = 'LIME'
-					else -- solid or unknown
-						tcolor = 'RED' 
-					end
+					tcolor = tiles.colors[game_tiles[ttype]]
+					tcolor = (tcolor ~= nil) and tcolor or "RED"
 				end
 				gui.drawBox((camx-x)%16-8+16*i,(camy-y)%16-16+16*j,(camx-x)%16+7+16*i,(camy-y)%16+16*j-1,tcolor)
 			end
@@ -201,5 +239,6 @@ while true do
 	gui.drawText(4,22,string.format("%X",gettile(x,y))..' '..string.format("%X",tileid(gettile(x,y))))
 	gui.drawText(3,31,"X:"..x.." Y:"..y,"BLACK","BLACK")
 	gui.drawText(4,32,"X:"..x.." Y:"..y,"WHITE")
+	gui.drawText(3,40,"Stage:"..stageid)
 	emu.frameadvance()
 end
